dd07e6e1dcb2580602f6c77155129ee5df7fbb76
sayedmisfar030 <123141754+sayedmisfar030@users.noreply.github.com>
2023-01-20 13:36:18 +0530

    Create sayed misfar.js

---
+++ sayed misfar.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript Arithmetic</h2>
+
+
+<p id="demo"></p>
+
+<script>
+let x = 100 + 50;
+document.getElementById("demo").innerHTML = x;
+</script>
+
+</body>
+</html>
